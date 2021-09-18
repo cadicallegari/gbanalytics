50626a5de4d49dee1b74997c84546c84e91a49ee
Claudinei Callegari <cadicallegari@gmail.com>
2021-09-18 17:37:45 +0200

    Handle cli params on main

--- cmd/gbanalytics/main.go
+++ cmd/gbanalytics/main.go
@@ -1,7 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var version = "dev" // it will be set on build time
+
+type config struct {
+	showVersion bool
+
+	// maybe it is better to have a single var for each file
+	dataPath string
+
+	// what is the question to be aswered
+	query string
+}
+
+func (cfg config) validate() error {
+	if cfg.showVersion {
+		return nil
+	}
+
+	if cfg.dataPath == "" {
+		return fmt.Errorf("missing data path parameter")
+	}
+
+	if cfg.query == "" {
+		return fmt.Errorf("missing query parameter")
+	}
+
+	return nil
+}
+
+func parseArgs() config {
+	var cfg config
+
+	flag.BoolVar(&cfg.showVersion, "version", false, "Show version")
+
+	flag.StringVar(&cfg.dataPath, "data", "", "where to find the files with the data")
+	flag.StringVar(&cfg.query, "query", "", "question to be aswered")
+
+	flag.Parse()
+
+	return cfg
+}
+
+func usage() {
+	fmt.Fprintf(
+		flag.CommandLine.Output(),
+		"parse data files and answer some questions. Version: %s\n",
+		version,
+	)
+
+	flag.PrintDefaults()
+}
 
 func main() {
-	fmt.Println("hello")
+	flag.Usage = usage
+
+	cfg := parseArgs()
+	if err := cfg.validate(); err != nil {
+		fmt.Println(err)
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if cfg.showVersion {
+		fmt.Println("version:", version)
+		return
+	}
+
+	fmt.Println("end")
 }
